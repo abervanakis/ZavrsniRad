29b09e7277f939dc3c08d82cb4865bdd6307093d
abervanakis <andreas2001ps4@gmail.com>
2023-09-12 18:14:15 +0200

    minor changes

--- src/main/java/zavrsnirad/posudbaopremeizlabosa/Main.java
+++ src/main/java/zavrsnirad/posudbaopremeizlabosa/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("PopisStudenataScene.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Posudba opreme iz laboratorija");
         stage.show();
     }
 
